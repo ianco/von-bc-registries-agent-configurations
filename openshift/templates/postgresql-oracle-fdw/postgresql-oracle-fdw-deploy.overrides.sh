8a33d61912a5c4660aa9504d5c7d33efb91ad16a
Wade Barnes <wade.barnes@shaw.ca>
2019-08-29 14:18:52 -0700

    Add postgresql-oracle-fdw build/deploy pipeline.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

---
+++ openshift/templates/postgresql-oracle-fdw/postgresql-oracle-fdw-deploy.overrides.sh
@@ -0,0 +1,28 @@
+.  $(find . -name overrides.inc)
+# ======================================================
+# Special Deployment Parameters needed for DB Deployment
+# ------------------------------------------------------
+# The results need to be encoded as OpenShift template
+# parameters for use with oc process.
+# ======================================================
+
+if createOperation; then
+  # Randomly generate a set of credentials without asking ...
+  printStatusMsg "Creating a set of random user credentials ..."
+  writeParameter "POSTGRESQL_USER" $(generateUsername) "false"
+  writeParameter "POSTGRESQL_PASSWORD" $(generatePassword) "false"
+  writeParameter "POSTGRESQL_ADMIN_PASSWORD" $(generatePassword) "false"
+  writeParameter "FDW_USER" $(generateUsername) "false"
+  writeParameter "FDW_PASS" $(generatePassword) "false"
+else
+  # Secrets are removed from the configurations during update operations ...
+  printStatusMsg "Update operation detected ...\nSkipping the generation of random user credentials ...\n"
+  writeParameter "POSTGRESQL_USER" "generation_skipped" "false"
+  writeParameter "POSTGRESQL_PASSWORD" "generation_skipped" "false"
+  writeParameter "POSTGRESQL_ADMIN_PASSWORD" "generation_skipped" "false"
+  writeParameter "FDW_USER" "generation_skipped" "false"
+  writeParameter "FDW_PASS" "generation_skipped" "false"
+fi
+
+SPECIALDEPLOYPARMS="--param-file=${_overrideParamFile}"
+echo ${SPECIALDEPLOYPARMS}
