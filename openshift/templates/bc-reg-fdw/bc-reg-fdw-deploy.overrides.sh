589121093b82de32ca8ba559164b8c29ac8f1517
Wade Barnes <wade.barnes@shaw.ca>
2019-08-30 13:00:27 -0700

    Refactor bc-reg-fdw and schema-spy-oracle deployment configurations.
    - Collect and store all of the foreign database configuration elements in the bc-reg-fdw deployment resources.
    - Source that same configuration information from the schema-spy-oracle deployment configuration.
    
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/templates/bc-reg-fdw/bc-reg-fdw-deploy.overrides.sh
+++ openshift/templates/bc-reg-fdw/bc-reg-fdw-deploy.overrides.sh
@@ -14,11 +14,13 @@ if createOperation; then
   writeParameter "POSTGRESQL_ADMIN_PASSWORD" $(generatePassword) "false"
   readParameter "FDW_USER - Please provide the username required to athenticate with the foreign database.\nIf left blank, a random username will be created however it will likely cause authentication to fail:" FDW_USER $(generateUsername) "false"
   readParameter "FDW_PASS - Please provide the password required to athenticate with the foreign database.\nIf left blank, a random password will be created however it will likely cause authentication to fail:" FDW_PASS $(generatePassword) "false"
-  readParameter "FDW_FOREIGN_SERVER - Please provide the URI of the foreign database.\nIt should be in the form <host>:<port>/<listener service name>.\nThe default is an empty string:" FDW_FOREIGN_SERVER "" "false"
+  readParameter "FDW_FOREIGN_HOST - Please provide the URI of the foreign database server.\nIt should be in the form <host>:<port>.\nThe default is an empty string:" FDW_FOREIGN_HOST "" "false"
+  readParameter "FDW_FOREIGN_NAME - Please provide the name of the foreign database.\nThe default is an empty string:" FDW_FOREIGN_NAME "" "false"
+  readParameter "FDW_FOREIGN_CATALOG - Please provide the name of the foreign catalog for the database.\nIt should be the <listener service name>.\nThe default is an empty string:" FDW_FOREIGN_CATALOG "" "false"
   readParameter "FDW_FOREIGN_SCHEMA - Please provide the name of the schema of the foreign database.\nThe default is an empty string:" FDW_FOREIGN_SCHEMA "" "false"
 else
   # Secrets are removed from the configurations during update operations ...
-  printStatusMsg "Update operation detected ...\nSkipping the generation of random user credentials ...\nSkipping the prompts for the FDW_USER, FDW_PASS, FDW_FOREIGN_SERVER, and FDW_FOREIGN_SCHEMA secrets ...\n"
+  printStatusMsg "Update operation detected ...\nSkipping the generation of random user credentials ...\nSkipping the prompts for the FDW_USER, FDW_PASS, FDW_FOREIGN_HOST, FDW_FOREIGN_NAME, FDW_FOREIGN_CATALOG, and FDW_FOREIGN_SCHEMA secrets ...\n"
   # Generated
   writeParameter "POSTGRESQL_USER" "generation_skipped" "false"
   writeParameter "POSTGRESQL_PASSWORD" "generation_skipped" "false"
@@ -26,7 +28,9 @@ else
   # Prompted
   writeParameter "FDW_USER" "generation_skipped" "false"
   writeParameter "FDW_PASS" "generation_skipped" "false"
-  writeParameter "FDW_FOREIGN_SERVER" "generation_skipped" "false"
+  writeParameter "FDW_FOREIGN_HOST" "generation_skipped" "false"
+  writeParameter "FDW_FOREIGN_NAME" "generation_skipped" "false"
+  writeParameter "FDW_FOREIGN_CATALOG" "generation_skipped" "false"
   writeParameter "FDW_FOREIGN_SCHEMA" "generation_skipped" "false"
 fi
 
