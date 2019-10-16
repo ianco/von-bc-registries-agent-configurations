eb475498e3e056d47d97d3d31c70cc17a83e2c57
Wade Barnes <wade.barnes@shaw.ca>
2019-10-16 11:51:10 -0700

    Update configurations
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- jenkins/db/config.groovy
+++ jenkins/db/config.groovy
@@ -4,6 +4,9 @@ class config extends bc.baseConfig {
   // Build configuration
   public static final String  APP_NAME = "db"
   public static final String[] BUILDS = ["${this.APP_NAME}"]
+
+  // Deployment configuration
+  public static final String[] DEPLOYMENTS = ["event-db", "event-processor-log-db", "wallet"]
 }
 
 return new config();
--- src/bc/baseConfig.groovy
+++ src/bc/baseConfig.groovy
@@ -1,4 +1,3 @@
-    
 package bc;
 
 class baseConfig {
