4e9b1a467cc4fe894afe7f36a652c221f3c0f755
Wade Barnes <wade.barnes@shaw.ca>
2020-11-02 14:41:55 -0800

    Update tools namespace
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- src/bc/baseConfig.groovy
+++ src/bc/baseConfig.groovy
@@ -8,7 +8,7 @@ class baseConfig {
   public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
 
   // The name of the project namespace(s).
-  public static final String  NAME_SPACE = 'devex-von-bc-registries-agent'
+  public static final String  NAME_SPACE = '7cba16'
 
   // Instance Suffix
   public static final String  SUFFIX = '-primary'
