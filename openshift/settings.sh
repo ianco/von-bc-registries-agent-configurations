bec070ffc6a81234881337f3ad9b7966f4d7c2f0
Wade Barnes <wade.barnes@shaw.ca>
2021-03-10 08:48:26 -0800

    Update settings
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,10 +1,9 @@
 export PROJECT_NAMESPACE="7cba16"
 export GIT_URI="https://github.com/bcgov/von-bc-registries-agent.git"
 export GIT_REF="master"
+export SKIP_PIPELINE_PROCESSING=1
 
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
-export -a skip_git_overrides="bc-reg-fdw-build.json backup-build.json"
-
-export ignore_templates="backup-deploy"
+export -a skip_git_overrides="bc-reg-fdw-build.yaml backup-build.yaml"
