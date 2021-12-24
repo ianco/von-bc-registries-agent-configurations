da499956d7bdb1d1e8ae4bc056f281a4f153563c
Wade Barnes <wade@neoterictech.ca>
2021-12-24 14:57:53 -0800

    Switch to using default git refs
    - Default branch has been renamed to `main`.
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,6 +1,6 @@
 export PROJECT_NAMESPACE="7cba16"
 export GIT_URI="https://github.com/bcgov/von-bc-registries-agent.git"
-export GIT_REF="master"
+export GIT_REF="main"
 
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
